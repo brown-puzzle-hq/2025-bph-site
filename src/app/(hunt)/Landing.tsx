181a512bc0f5158b11ae8f99ad63520cfe31a5a7
Alex Wang <alexander_wang@brown.edu>
2025-01-27 05:25:57 -0500

    Better parallax

--- src/app/(hunt)/Landing.tsx
+++ src/app/(hunt)/Landing.tsx
@@ -1,16 +1,11 @@
 "use client";
 import { useEffect, useState } from "react";
-import Link from "next/link";
 import { IN_PERSON, REMOTE } from "~/hunt.config";
 
 const formatter = new Intl.DateTimeFormat("en-US", {
-  weekday: "long",
-  year: "numeric",
-  month: "long",
+  year: "2-digit",
+  month: "numeric",
   day: "numeric",
-  hour: "numeric",
-  minute: "numeric",
-  timeZoneName: "short",
 });
 
 export default function Landing() {
@@ -26,8 +21,8 @@ export default function Landing() {
   }, []);
 
   return (
-    <div className="relative w-screen overflow-hidden bg-mahogany-950">
-      <div className="relative h-[100vh] w-screen overflow-hidden bg-mahogany-950">
+    <div className="relative w-screen overflow-hidden">
+      <div className="relative h-[100vh] w-screen">
         {/* absolute background with stars */}
         <div
           className="absolute inset-0 h-[170vh] w-full bg-cover bg-top"
@@ -67,103 +62,40 @@ export default function Landing() {
             clipPath: `inset-0`,
           }}
         />
-        <div className="text-primary relative mt-6 w-full bg-mahogany-950 p-4">
-          <h1>Hunt Information</h1>
-          <h2>What is Brown Puzzlehunt?</h2>
-        </div>
       </div>
 
       {/* Div right below the image */}
-      <div className="text-primary relative mt-6 w-full bg-mahogany-950 p-4">
-        <h1>Hunt Information</h1>
-        <h2>What is Brown Puzzlehunt?</h2>
-        <p>
-          Brown Puzzlehunt 2025 is the third annual puzzlehunt run by current
-          Brown and RISD students.
-        </p>
-        <p>
-          In a puzzlehunt, participants compete in teams to solve puzzles.
-          Puzzles can come in many different forms; the only commonality is that
-          there are usually no direct instructions, so it is up to you to
-          extract an English word or phrase from the information given. You can
-          read a longer introduction to puzzlehunts{" "}
-          <Link
-            href="https://blog.vero.site/post/puzzlehunts"
-            className="no-underline hover:underline"
-          >
-            here
-          </Link>
-          .
-        </p>
-        <p>
-          This hunt is designed to be a relatively easy introduction to
-          puzzlehunting for beginner teams. We expect this year's Brown
-          Puzzlehunt to be roughly the same length and difficulty as last year's{" "}
-          <Link
-            href="https://2024.brownpuzzlehunt.com/"
-            className="no-underline hover:underline"
-          >
-            hunt
-          </Link>
-          . If you are looking for some practice, we recommend looking at
-          puzzles from other online hunts such as{" "}
-          <Link
-            href="https://galacticpuzzlehunt.com/"
-            className="no-underline hover:underline"
-          >
-            Galactic Puzzle Hunt
-          </Link>
-          ,{" "}
-          <Link
-            href="https://teammatehunt.com/"
-            className="no-underline hover:underline"
-          >
-            Teammate Hunt
-          </Link>
-          , or{" "}
-          <Link
-            href="https://puzzlepotluck.com/"
-            className="no-underline hover:underline"
-          >
-            Puzzle Potluck
-          </Link>
-          .
-        </p>
-        <h2>When and where is this happening?</h2>
-        <p>
-          This year, Brown Puzzlehunt is offering three different ways to
-          participate across two weekends. You can hunt in-person, hunt fully
-          remotely, or purchase a Box.
-        </p>
-        <h3>In-Person Event</h3>
-        <p>
-          If you come to campus, you'll get to do events, physical puzzles,
-          interactions, and the in-person runaround! The In-Person Event will
-          run from {formatter.format(IN_PERSON.KICKOFF_TIME)} to{" "}
-          {formatter.format(IN_PERSON.END_TIME)}, at{" "}
-          <strong>Brown University</strong> in{" "}
-          <strong>Providence, Rhode Island</strong>.
-        </p>
-        <p>
-          <strong>All in-person participants </strong>
-          need to print and fill out{" "}
-          <Link
-            href="https://studentactivities.brown.edu/sites/default/files/safety/Physical%20Activity%20Release.pdf"
-            className="no-underline hover:underline"
-          >
-            this waiver
-          </Link>{" "}
-          ahead of time in order to come to campus. We will collect these at
-          kickoff.
-        </p>
-
-        <h3>Remote Event</h3>
-        <p>
-          If you can't come to Brown's campus, you can do our online-only event.
-          This runs from {formatter.format(REMOTE.START_TIME)} to{" "}
-          {formatter.format(REMOTE.END_TIME)}. (This is a different weekend than
-          the In-Person Event!)
-        </p>
+      <div className="flex justify-center pt-[calc((100vw-850px)/8)]">
+        <div className="relative flex p-4 text-center w-[calc(60vw+200px)]">
+          <div className="w-1/3 p-2 md:p-4">
+            <h1>What</h1>
+            <p>
+              The third annual puzzlehunt run by current Brown and RISD
+              students.
+            </p>
+          </div>
+          <div className="w-1/3 p-2 md:p-4">
+            <h1>When</h1>
+            <p>
+              In-Person:{" "}
+              <span className="whitespace-nowrap">
+                {formatter.format(IN_PERSON.START_TIME)} –
+                {formatter.format(IN_PERSON.END_TIME)}
+              </span>
+            </p>
+            <p>
+              Remote:{" "}
+              <span className="whitespace-nowrap">
+                {formatter.format(REMOTE.START_TIME)} –
+                {formatter.format(REMOTE.END_TIME)}
+              </span>
+            </p>
+          </div>
+          <div className="w-1/3 p-2 md:p-4">
+            <h1>Who</h1>
+            <p>Anyone, anywhere in the world.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
