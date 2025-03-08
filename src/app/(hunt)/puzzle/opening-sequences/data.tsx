ce51b61069cf3e944c6901e1185d028731c01cfe
alex-j-wang <alex-j-wang@users.noreply.github.com>
2025-03-08 05:20:48 +0000

    Apply formatting changes

--- src/app/(hunt)/puzzle/opening-sequences/data.tsx
+++ src/app/(hunt)/puzzle/opening-sequences/data.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import PUZZLE from "./OP_Puzzle_Body.jpg"
+import PUZZLE from "./OP_Puzzle_Body.jpg";
 
 /**
  * The puzzle ID is used to uniquely identify the puzzle in the database.
@@ -15,7 +15,12 @@ export const puzzleId = "opening-sequences";
  */
 export const inPersonBody = (
   <div className="max-w-3xl text-center">
-    <Image src={PUZZLE} width={500} height={500} alt="chess tree (puzzle body) image"/>
+    <Image
+      src={PUZZLE}
+      width={500}
+      height={500}
+      alt="chess tree (puzzle body) image"
+    />
   </div>
 );
 
@@ -23,7 +28,12 @@ export const remoteBoxBody = inPersonBody;
 
 export const remoteBody = (
   <div className="max-w-3xl text-center">
-    <Image src={PUZZLE} width={500} height={500} alt="chess tree (puzzle body) image"/>
+    <Image
+      src={PUZZLE}
+      width={500}
+      height={500}
+      alt="chess tree (puzzle body) image"
+    />
   </div>
 );
 
@@ -32,32 +42,58 @@ export const remoteBody = (
  * If there are no solutions available, set it null.
  */
 export const solutionBody = (
-  <div className="max-w-3xl text-left space-y-4">
+  <div className="max-w-3xl space-y-4 text-left">
     <p>
-    The goal of this puzzle is to identify the names of the chess positions, which are different openings. The lines between the positions and the number indicate the number of moves (from white or black) it takes to go from one board state to the other. No number indicates one move.
-  </p>
+      The goal of this puzzle is to identify the names of the chess positions,
+      which are different openings. The lines between the positions and the
+      number indicate the number of moves (from white or black) it takes to go
+      from one board state to the other. No number indicates one move.
+    </p>
 
-  <p className="space-y-4">The names of the positions with indices are (top to bottom):</p>
-  
-  <ul className="list-disc list-inside space-y-2">
-    <li>Nescafe Frappe Attack <span className="font-semibold">[12]</span></li>
-    <li>Maccutcheon Variation <span className="font-semibold">[2]</span></li>
-    <li>Winawer Variation <span className="font-semibold">[1]</span></li>
-    <li>Bongcloud Attack <span className="font-semibold">[3]</span></li>
-    <li>Trompowsky Attack <span className="font-semibold">[1]</span></li>
-    <li>Petrov Defence <span className="font-semibold">[5]</span></li>
-    <li>Fingerslip Variation <span className="font-semibold">[1]</span></li>
-    <li>4 Knights Attack <span className="font-semibold">[1]</span></li>
-  </ul>
+    <p className="space-y-4">
+      The names of the positions with indices are (top to bottom):
+    </p>
 
-  <p>
-    Taking indices one obtains <span className="font-bold text-blue-400">PAWNTOF4</span> (4). This move gives rise to the Bird’s Opening. Since the answer is 4 letters, we get 
-    <span className="font-bold text-blue-400"> BIRD</span>.
-  </p>
+    <ul className="list-inside list-disc space-y-2">
+      <li>
+        Nescafe Frappe Attack <span className="font-semibold">[12]</span>
+      </li>
+      <li>
+        Maccutcheon Variation <span className="font-semibold">[2]</span>
+      </li>
+      <li>
+        Winawer Variation <span className="font-semibold">[1]</span>
+      </li>
+      <li>
+        Bongcloud Attack <span className="font-semibold">[3]</span>
+      </li>
+      <li>
+        Trompowsky Attack <span className="font-semibold">[1]</span>
+      </li>
+      <li>
+        Petrov Defence <span className="font-semibold">[5]</span>
+      </li>
+      <li>
+        Fingerslip Variation <span className="font-semibold">[1]</span>
+      </li>
+      <li>
+        4 Knights Attack <span className="font-semibold">[1]</span>
+      </li>
+    </ul>
 
-  <p className="italic text-gray-500">
-    NOTE: The last opening could alternatively be “Four Knights Attack,” which gives an F during indexing, but PAWNTOFF doesn’t make sense, so the puzzle solver is expected to realize you are supposed to index the number 4 instead.
-  </p>
+    <p>
+      Taking indices one obtains{" "}
+      <span className="font-bold text-blue-400">PAWNTOF4</span> (4). This move
+      gives rise to the Bird’s Opening. Since the answer is 4 letters, we get
+      <span className="font-bold text-blue-400"> BIRD</span>.
+    </p>
+
+    <p className="italic text-gray-500">
+      NOTE: The last opening could alternatively be “Four Knights Attack,” which
+      gives an F during indexing, but PAWNTOFF doesn’t make sense, so the puzzle
+      solver is expected to realize you are supposed to index the number 4
+      instead.
+    </p>
   </div>
 );
 
