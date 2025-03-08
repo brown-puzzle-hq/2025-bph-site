96aa61f261dd0c1f6eb896f4a383104d0e14069f
Chloe Qiao <qiaochloe@gmail.com>
2025-03-07 21:15:47 -0500

    Add heist

---
+++ src/app/api/heist/route.ts
@@ -0,0 +1,42 @@
+import fs from "fs";
+import path from "path";
+import { auth } from "~/server/auth/auth";
+import { NextResponse } from "next/server";
+import { canViewPuzzle } from "~/app/(hunt)/puzzle/actions";
+
+export async function GET() {
+  // Authentication
+  const session = await auth();
+  const viewStatus = await canViewPuzzle("heist", session);
+  if (viewStatus !== "success") {
+    return new NextResponse(null, { status: 404 });
+  }
+
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "app",
+    "(hunt)",
+    "puzzle",
+    "heist",
+    "heist.html",
+  );
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      return new NextResponse("File not found", { status: 404 });
+    }
+
+    const fileContent = fs.readFileSync(filePath, "utf8");
+
+    return new NextResponse(fileContent, {
+      headers: {
+        "Content-Type": "text/html",
+        "Content-Disposition": "inline; filename=heist.html",
+      },
+    });
+  } catch (error) {
+    console.error("Error loading file.");
+    return new NextResponse("Error");
+  }
+}
