abae364e2323c9d63a9f764b938491a9be9db8af
mcarls10 <megan_carlson@brown.edu>
2025-03-08 01:12:08 -0500

    more puzzlesssss

---
+++ src/app/(hunt)/puzzle/youve-got-this-covered/data.tsx
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+/**
+ * The puzzle ID is used to uniquely identify the puzzle in the database.
+ * It should be equal to the name of the folder this file is currently under.
+ * Feel free to make this creative, because the route to the puzzle will be
+ * example.com/puzzle/puzzleId.
+ */
+export const puzzleId = "youve-got-this-covered";
+
+/**
+ * The body renders above the guess submission form. Put flavor text, images,
+ * and interactive puzzle components here.
+ */
+export const inPersonBody = (
+  <div>
+    <div className="max-w-3xl text-center mb-6">This is a physical puzzle! If your team has not already picked up a CD, please visit HQ in Friedman 208.</div>
+    <hr className="border-t border-white mb-6 my-6" />
+  </div>
+);
+
+export const remoteBoxBody = (<div>
+  <div className="max-w-3xl text-center"><i>This is a physical puzzle! You should have received it in your Box. Contact brownpuzzlehq@gmail.com with any questions about your Box or its materials.</i></div>
+  <hr className="border-t border-white mb-6 my-6" />
+</div>
+
+);
+
+export const remoteBody = (
+  <div>
+    <div className="max-w-3xl mt-8 text-center">
+      <Link href="https://drive.google.com/file/d/149vQ1n_JK39qX7UsFA0SsRLI5vzFO0iH/view?usp=sharing"><span className="underline">[audio]</span></Link>
+      </div>
+    {/* i do not know how to embed audio and cannot figure it out so this can be fixed later */}
+  </div>
+);
+
+/**
+ * The `solutionBody` renders in the solution page.
+ * If there are no solutions available, set it null.
+ */
+export const solutionBody = (
+  <div className="max-w-3xl text-center">This solution is sorta written but like...fill this in later :)</div>
+);
+
+/**
+ * The `copyText` should provide a convenient text representation of the puzzle
+ * that can be copied to the clipboard. Set this to `null` to remove the copy button.
+ */
+export const copyText = `null`;
+
+/**
+ * The `partialSolutions` object is used to prompt solutions with significant progress.
+ * Each key is a partial solution, and the value is the prompt to be displayed. Keys must
+ * be in all caps, no spaces.
+ */
+export const partialSolutions: Record<string, string> = {};
+
+/**
+ * The `tasks` object is used for multi-part puzzles. When a certain answer is submitted,
+ * more content will be added to the puzzle body. Keys must be in all caps, no spaces.
+ */
+export const tasks: Record<string, JSX.Element> = {};
