225779d1c961aeb95bea383ad520e396a0115457
Arnav Singhal <arnav.b.singhal@gmail.com>
2025-03-08 02:13:22 -0500

    guard and door

--- src/app/(hunt)/puzzle/the-guard-and-the-door/data.tsx
+++ src/app/(hunt)/puzzle/the-guard-and-the-door/data.tsx
@@ -1,36 +1,29 @@
-import Image from "next/image";
-import HL_1 from "./hl-1.png";
-import HL_2 from "./hl-2.png";
-import HL_3 from "./hl-3.png";
-import HL_4 from "./hl-4.png";
-import HL_5 from "./hl-5.png";
-import HL_6 from "./hl-6.png";
-const SCALE = 0.5;
-
 /**
  * The puzzle ID is used to uniquely identify the puzzle in the database.
  * It should be equal to the name of the folder this file is currently under.
  * Feel free to make this creative, because the route to the puzzle will be
  * example.com/puzzle/puzzleId.
  */
-export const puzzleId = "hand-letters";
+export const puzzleId = "the-guard-and-the-door";
 
 /**
  * The body renders above the guess submission form. Put flavor text, images,
  * and interactive puzzle components here.
  */
-
 export const inPersonBody = (
-  <div className="max-w-3xl text-center">
-  <p className="mb-4 italic">I'm on this really exciting movie shoot, but I've become so crazed that
-  whenever I look at the clocks, I think they're trying to give me a message!
-  I just wish I knew where I am heading next...</p>
-  <Image src={HL_1} alt="" width={666*SCALE} height={454*SCALE} className="mb-5"/>
-  <Image src={HL_2} alt="" width={484*SCALE} height={366*SCALE} className="mb-5"/>
-  <Image src={HL_3} alt="" width={580*SCALE} height={388*SCALE} className="mb-5"/>
-  <Image src={HL_4} alt="" width={660*SCALE} height={524*SCALE} className="mb-5"/>
-  <Image src={HL_5} alt="" width={534*SCALE} height={258*SCALE} className="mb-5"/>
-  <Image src={HL_6} alt="" width={580*SCALE} height={692*SCALE} className="mb-5"/>
+  <div className="max-w-3xl">
+  <p className="mb-8 italic">As you approach the end of Brown Puzzlehunt, you find in front of you The Guard and The Door.
+  <br></br>The Guard tells you the following:</p>
+  <p className="mb-4">“Upon observation, when viewing it all,
+  <br></br>how many are spotted right there on the wall?</p>
+  <p className="mb-4">“Once the second one comes near, and if the truth is all you hear,
+  <br></br>you must make it very clear the chance your letters are top tier.</p>
+  <p className="mb-4">“In the past few moves, I need not see a thing,
+  <br></br>but I now must observe to decide what to bring.</p>
+  <p className="mb-4">“Not the moment you march straight through a door,
+  <br></br>but the time that you get four chances before.</p>
+  <p className="mb-4">“When they both point, they will point right or wrong,
+  <br></br>find how many are right, and you'll move right along.”</p>
   </div>
 );
 
@@ -50,12 +43,23 @@ export const solutionBody = (
  * The `copyText` should provide a convenient text representation of the puzzle
  * that can be copied to the clipboard. Set this to `null` to remove the copy button.
  */
-export const copyText = `Providence\tPonta Delgata
-Hanoi\tRome
-Tehran\tTokyo
-New Delhi\tDhaka\tLondon
-Helsinki
-Adelaide\tBrisbane\tVladivostok`;
+export const copyText = `As you approach the end of Brown Puzzlehunt, you find in front of you The Guard and The Door.
+The Guard tells you the following:
+
+“Upon observation, when viewing it all,
+how many are spotted right there on the wall?
+
+“Once the second one comes near, and if the truth is all you hear,
+you must make it very clear the chance your letters are top tier.
+
+“In the past few moves, I need not see a thing,
+but I now must observe to decide what to bring.
+
+“Not the moment you march straight through a door,
+but the time that you get four chances before.
+
+“When they both point, they will point right or wrong,
+find how many are right, and you'll move right along.”`;
 
 /**
  * The `partialSolutions` object is used to prompt solutions with significant progress.
