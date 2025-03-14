987fb93056a5e79a8944fd2f84167baee888faaa
Chloe Qiao <qiaochloe@gmail.com>
2025-03-14 02:00:59 -0400

    Make map not reload between tabs

--- src/app/(hunt)/puzzle/page.tsx
+++ src/app/(hunt)/puzzle/page.tsx
@@ -4,14 +4,8 @@ import Link from "next/link";
 import { db } from "@/db/index";
 import { eq, inArray } from "drizzle-orm";
 import { solves, puzzles, unlocks, answerTokens } from "~/server/db/schema";
-import PuzzleTable from "./components/PuzzleTable";
-import EventTable from "./components/EventTable";
 import { Round, ROUNDS } from "@/hunt.config";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import dynamic from "next/dynamic";
-import { MapIcon, Table } from "lucide-react";
-
-const Map = dynamic(() => import("./Map"), { ssr: false });
+import PuzzleListPage from "../components/PuzzleListPage";
 
 export default async function Home() {
   const session = await auth();
@@ -104,76 +98,40 @@ export default async function Home() {
     ),
   })).filter((round) => round.puzzles.length > 0);
 
+  const canSeeEvents =
+    currDate > REMOTE.END_TIME ||
+    (session?.user &&
+      session.user.interactionMode === "in-person" &&
+      currDate > IN_PERSON.START_TIME);
+
+  if (!canSeeEvents) return;
+
+  const availableEvents: {
+    id: string;
+    name: string;
+    answer: string;
+    description: string;
+    startTime: Date;
+  }[] = await db.query.events.findMany();
+
+  const finishedEvents: {
+    eventId: string;
+    puzzleId: string | null;
+  }[] = session?.user
+    ? await db.query.answerTokens.findMany({
+        where: eq(answerTokens.teamId, session.user?.id!),
+      })
+    : [];
+
   return (
-    <Tabs defaultValue="map">
-      <TabsList className="fixed z-20 right-0 m-2 space-x-1 py-5 bg-footer-bg text-[#6c518e]">
-        <TabsTrigger
-          className="data-[state=active]:bg-[#5e437e] data-[state=active]:text-main-text"
-          value="map"
-        >
-          <MapIcon />
-        </TabsTrigger>
-        <TabsTrigger
-          className="data-[state=active]:bg-[#5e437e] data-[state=active]:text-main-text"
-          value="tables"
-        >
-          <Table />
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="map" className="mt-0">
-        <Map
-          availablePuzzles={availablePuzzles}
-          solvedPuzzles={solvedPuzzles}
-        />
-      </TabsContent>
-      <TabsContent value="tables" className="mt-0">
-        <div className="mx-auto mb-6 flex w-full max-w-3xl grow flex-col items-center p-4 pt-6">
-          <h1 className="mb-2">Puzzles</h1>
-          <PuzzleTable
-            availableRounds={availableRounds}
-            availablePuzzles={availablePuzzles}
-            solvedPuzzles={solvedPuzzles}
-          />
-          {(async () => {
-            // Check if user should see the events
-            const canSeeEvents =
-              currDate > REMOTE.END_TIME ||
-              (session?.user &&
-                session.user.interactionMode === "in-person" &&
-                currDate > IN_PERSON.START_TIME);
-
-            if (!canSeeEvents) return;
-
-            const availableEvents: {
-              id: string;
-              name: string;
-              answer: string;
-              description: string;
-              startTime: Date;
-            }[] = await db.query.events.findMany();
-
-            const finishedEvents: {
-              eventId: string;
-              puzzleId: string | null;
-            }[] = session?.user
-              ? await db.query.answerTokens.findMany({
-                  where: eq(answerTokens.teamId, session.user?.id!),
-                })
-              : [];
-
-            return (
-              <>
-                <h1 className="mb-2 mt-4">Events</h1>
-                <EventTable
-                  availableEvents={availableEvents}
-                  finishedEvents={finishedEvents}
-                  inputBox={!!session?.user}
-                />
-              </>
-            );
-          })()}
-        </div>
-      </TabsContent>
-    </Tabs>
+    <PuzzleListPage
+      availablePuzzles={availablePuzzles}
+      solvedPuzzles={solvedPuzzles}
+      availableRounds={availableRounds}
+      canSeeEvents={canSeeEvents}
+      availableEvents={availableEvents}
+      finishedEvents={finishedEvents}
+      hasEventInputBox={!!session?.user}
+    />
   );
 }
