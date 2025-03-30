1fcfc03df7183f756e8bc0f0466d01d6877cd6e5
Alex Wang <alexander_wang@brown.edu>
2025-03-29 23:03:20 -0400

    Move metadata

--- src/app/(hunt)/puzzle/(adventure)/m-guards-n-doors-and-k-choices/page.tsx
+++ src/app/(hunt)/puzzle/(adventure)/m-guards-n-doors-and-k-choices/page.tsx
@@ -1,6 +1,5 @@
 import DefaultPuzzlePage from "@/puzzle/components/DefaultPuzzlePage";
 import * as data from "./data";
-
 import { db } from "~/server/db";
 import { eq, sql } from "drizzle-orm";
 import { mnk } from "~/server/db/schema";
@@ -8,20 +7,6 @@ import { auth } from "~/server/auth/auth";
 import RemoteBody from "./RemoteBody";
 import { redirect } from "next/navigation";
 
-export const metadata = {
-  title: data.puzzleId
-    .split("-")
-    .map((word) => {
-      // Uppercase every letter in a roman numeral
-      const romanRegex =
-        /^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
-      if (romanRegex.test(word.toUpperCase())) {
-        return word.toUpperCase();
-      }
-      return word.charAt(0).toUpperCase() + word.slice(1);
-    })
-    .join(" "),
-};
 
 export default async function Page({
   searchParams,
--- src/app/(hunt)/puzzle/(cerebral)/the-guard-and-the-door/layout.tsx
+++ src/app/(hunt)/puzzle/(cerebral)/the-guard-and-the-door/layout.tsx
@@ -1,6 +1,21 @@
 import { puzzleId, solutionBody } from "./data";
 import DefaultHeader from "@/puzzle/components/DefaultHeader";
 
+export const metadata = {
+  title: puzzleId
+    .split("-")
+    .map((word) => {
+      // Uppercase every letter in a roman numeral
+      const romanRegex =
+        /^(M{0,4})(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/;
+      if (romanRegex.test(word.toUpperCase())) {
+        return word.toUpperCase();
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join(" ") + " - Brown Puzzlehunt",
+};
+
 export default async function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
