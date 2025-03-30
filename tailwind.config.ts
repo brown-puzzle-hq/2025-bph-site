6ac467c603dfeb52dbc8a4a4e5e42cfc7e016475
Alex Wang <alexander_wang@brown.edu>
2025-03-30 19:40:46 -0400

    Puzzle graph shake

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -129,9 +129,9 @@ export default {
       keyframes: {
         shake: {
           "0%, 100%": { transform: "translateX(0)" },
-          "25%": { transform: "translateX(-5px)" },
+          "25%": { transform: "translateX(-3px)" },
           "50%": { transform: "translateX(3px)" },
-          "75%": { transform: "translateX(-5px)" },
+          "75%": { transform: "translateX(-3px)" },
         },
       },
       animation: {
