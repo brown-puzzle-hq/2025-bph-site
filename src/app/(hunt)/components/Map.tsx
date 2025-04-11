6b53aa30a7679255afdb533e0e734e6eb07760ef
JackSharks05 <jacksharks05@gmail.com>
2025-04-11 11:05:28 -0400

    added drama section, added Gray, made clear meta pngs (they click the buildings)

--- src/app/(hunt)/components/Map.tsx
+++ src/app/(hunt)/components/Map.tsx
@@ -22,16 +22,16 @@ const positions: Record<string, L.LatLngExpression> = {
   "a-fistful-of-cards-ii": [540, 240],
   "a-fistful-of-cards-iii": [480, 650],
   "a-fistful-of-cards-iv": [650, 473],
-  "aha-erlebnis": [480, 160],
+  "aha-erlebnis": [470, 160],
   "are-you-sure": [490, 880],
-  "balloon-animals": [300, 580],
+  "balloon-animals": [290, 580],
   beads: [490, 230],
   "bluenos-puzzle-box": [653, 305],
-  "boring-plot": [735, 365],
+  "boring-plot": [720, 365],
   "chain-letters": [400, 700],
   "color-transfer": [580, 560],
   constellation: [560, 390],
-  "cutting-room-floor": [570, 490],
+  "cutting-room-floor": [560, 490],
   "drop-the": [420, 413],
   "eye-of-the-storm": [760, 500],
   "eye-spy": [655, 705],
@@ -62,7 +62,8 @@ const positions: Record<string, L.LatLngExpression> = {
   plagiarism: [335, 460],
   "red-blue": [460, 510],
   "secret-ingredient": [610, 570],
-  "six-degrees": [542, 700],
+  "six-degrees": [522, 700],
+  // "six-degrees": [542, 700],
   "international-neighbors": [560, 780],
   "ten-guards-ten-doors": [480, 310],
   "the-compact-disc": [540, 620],
@@ -111,7 +112,7 @@ export default function Map({
       <MapContainer
         center={[500, 500]}
         zoom={2}
-        minZoom={1.25}
+        minZoom={-1.25}
         maxZoom={3.5}
         maxBounds={bounds}
         crs={L.CRS.Simple}
@@ -124,10 +125,10 @@ export default function Map({
       >
         <ImageOverlay url={colorlayout} bounds={bounds} /> {/*/TODO: remove */}
         {/* <ImageOverlay url={layouts.Action!} bounds={bounds} />
-        <ImageOverlay url={layouts.Drama!} bounds={bounds} />
         <ImageOverlay url={layouts.Comedy!} bounds={bounds} />
         <ImageOverlay url={layouts.Adventure!} bounds={bounds} />
         <ImageOverlay url={layouts.Cerebral!} bounds={bounds} /> */}
+        <ImageOverlay url={layouts.Drama!} bounds={bounds} />
         <ImageOverlay url={layouts.Reality!} bounds={bounds} />
         {availablePuzzles.map((puzzle) => (
           <Marker
