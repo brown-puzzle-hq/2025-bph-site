aaae795f07a1c20611a2091ddba61ae14e27b156
Chloe Qiao <qiaochloe@gmail.com>
2025-04-11 22:47:25 -0400

    Make more elaborate Discord channel system

--- src/lib/comms.ts
+++ src/lib/comms.ts
@@ -8,19 +8,34 @@ export function extractEmails(memberString: string): string[] {
     .filter(Boolean);
 }
 
-export async function sendBotMessage(message: string) {
-  if (!process.env.DISCORD_WEBHOOK_URL) return;
+type Channel = "general" | "feedback" | "dev";
+
+// TODO: change later
+const channelToWebhookURL: Record<Channel, string | undefined> = {
+  general: process.env.DISCORD_WEBHOOK_URL,
+  feedback: process.env.DISCORD_WEBHOOK_URL,
+  dev: process.env.DISCORD_WEBHOOK_URL,
+};
+
+export async function sendBotMessage(
+  message: string,
+  channel: Channel = "general",
+) {
+  const webhookURL = channelToWebhookURL[channel];
+  // Disable the webhook by not including it in the env file
+  if (!webhookURL) return;
+
   if (message.length > 2000) {
     const chunks = message.match(/[\s\S]{1,2000}/g);
     if (chunks) {
       for (const chunk of chunks) {
-        await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+        await axios.post(webhookURL, {
           content: chunk,
         });
       }
     }
   } else {
-    await axios.post(process.env.DISCORD_WEBHOOK_URL, {
+    await axios.post(webhookURL, {
       content: message,
     });
   }
