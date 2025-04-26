8f54bf1b25e703dce1fb83100729d48b40c6f76b
Alex Wang <alexander_wang@brown.edu>
2025-04-26 18:48:22 -0400

    Add default authors

--- src/app/(hunt)/puzzle/(comedy)/balloon-animals/data.tsx
+++ src/app/(hunt)/puzzle/(comedy)/balloon-animals/data.tsx
@@ -129,6 +129,11 @@ Your circus movie is pretty good, but it requires more mainstream
 export const solutionBody = null;
 
 /**
+ * The `authors` string renders below the `solutionBody`.
+ */
+export const authors = null;
+
+/**
  * The `copyText` should provide a convenient text representation of the puzzle
  * that can be copied to the clipboard. Set this to `null` to remove the copy button.
  */
